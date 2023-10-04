c36a730ac4ddb580a7b3829bfe8e65cb3954c2b3
bernhard_auer <bernhard.auer@world-direct.at>
2023-10-05 00:12:45 +0200

    wrong usage of config?

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,15 +6,17 @@ namespace Az_Schulung.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
-
+            ViewData.Add("bgColor", _configuration["Feature1:backgroudColor"]);
         }
     }
 }
