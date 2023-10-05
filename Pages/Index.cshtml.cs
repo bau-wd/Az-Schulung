8cee24e636c49731cead71421dfb6508e806c4f3
bernhard_auer <bernhard.auer@world-direct.at>
2023-10-05 16:13:14 +0200

    add new feature

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,14 +6,18 @@ namespace Az_Schulung.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
+            ViewData.Add("welcomeText", _configuration["Feature1:WelcomeText"]);
+            ViewData.Add("textColor", _configuration["Feature1:TextColor"]);
 
         }
     }
